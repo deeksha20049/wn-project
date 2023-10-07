a5854889cc0f7bd4307daded030df526f1381e16
deeksha20049 <deeksha20049@iiitd.ac.in>
2023-10-07 21:30:31 +0530

    mobility logic

--- mobility.py
+++ mobility.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 class RandomWaypointModel:
     def __init__(self, area_width, area_height, max_speed, min_pause, max_pause):
@@ -14,4 +15,17 @@ class RandomWaypointModel:
         self.speed = 0
         self.pause_time = 0
 
-    
+    def updatePositions(self, time_step):
+        if self.pause_time > 0:
+            self.pause_time -= time_step
+        else:
+            distance = math.sqrt(math.pow((self.target_x-self.current_x),2) + math.pow((self.target_y-self.current_y),2))
+            if distance > 0:
+                self.speed = random.uniform(0, self.max_speed)
+                ratio = self.speed / distance
+                self.current_x += (self.target_x - self.current_x) * ratio
+                self.current_y += (self.target_y - self.current_y) * ratio
+
+            self.pause_time = random.uniform(self.min_pause, self.max_pause)
+            self.target_x = random.uniform(0, self.area_width)
+            self.target_y = random.uniform(0, self.area_height)
